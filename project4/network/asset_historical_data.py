90ad22703997ea17e8853fca70501e268cc977ca
foongzyy <foongzy@gmail.com>
2021-03-05 13:20:00 +0800

    second commit

--- project4/network/asset_historical_data.py
+++ project4/network/asset_historical_data.py
@@ -1,5 +1,11 @@
+#Retrieving data
 import requests as r
 import json
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 asset_url='https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance'
 
 headers={
@@ -7,7 +13,7 @@ headers={
     'Content-Type': 'application/json',
 }
 
-data=json.loads(r.post(history_url,headers=headers).content)
+data_raw=json.loads(r.post(history_url,headers=headers).content)
 
 #sorting data
 assetSymbol=(data_raw[0]['assetSymbol'])
@@ -17,3 +23,23 @@ for i in range(0,len(data)):
     price_data.append(data_raw[i]['price'])
     temp=datetime.datetime.fromtimestamp(data_raw[i]['timestamp'])
     time_data.append(temp)
+
+#putting into pandas
+pd.to_datetime(time_data)
+temp_dic={'datetime':time_data,'price':price_data}
+
+df=pd.DataFrame(temp_dic)
+#set datetime as index
+df=df.set_index('datetime')
+
+#SMA
+######################Set SMA Cycles here#######################
+cycles=1
+
+#initialise storage
+cleandata_mean_SMA=[]
+
+cleandata_mean_SMA=df['price'].rolling(window=cycles).mean()
+
+cleandata_mean_SMA
+cleandata_mean_SMA.head(50)
